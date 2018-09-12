2b697b1e3276d84b6d068805330303b42a7cbb14
hdy <378759617@qq.com>
2018-09-12 17:09:16 +0800

    登录依旧有问题

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import datetime
 from urllib import parse
 import json
 import random
+from urllib.parse import quote
+import  string
+import time
 # 当前的session会话对象
 session = requests.session()
 
@@ -26,16 +29,16 @@ def login(username, password):
     # 从html当中获取时间
     data_keys_time = ul['data-keys-time']
     r = session.get("https://www.yiban.cn/captcha/index?" +
-                    (str(datetime.datetime.today())))
-    # r = session.get("https://www.yiban.cn/captcha/index?" +
-    #                 (str(datetime.datetime.today())))
+                    str(int(time.time())))
+    r = session.get("https://www.yiban.cn/captcha/index?" +
+                    (str(int(time.time()))))
     # 查找是否已经存在验证码
-    captcha = soup.find_all("img", class_="captcha")
     with open("/Users/hdy/Desktop/yanzhengma.jpg", 'wb') as f:
             f.write(r.content)
     code = input("请输入验证码")
     # 获取到验证码
-    code = code.encode('unicode_escape')
+    # code = code.encode('unicode_escape')
+    code = quote(code, safe = string.printable)
     print(code)
     encrypt_password = get_crypt_password(data_keys, password)
     form_data = {
@@ -44,6 +47,7 @@ def login(username, password):
         'captcha': code,
         'keysTime': data_keys_time
     }
+    print(form_data)
     # 进行模拟登陆
     r = session.post("https://www.yiban.cn/login/doLoginAjax", data=form_data,allow_redirects=False)
     # 获取到返回的json数据
@@ -63,6 +67,35 @@ def login(username, password):
         addYiMiaoMiao()
     else:
         print("错误码:"+login_json['code']+" 原因:"+login_json['message'])
+        if(login_json['code'] == '711'):
+            r = session.get("https://www.yiban.cn/captcha/index?" +
+                    (str(int(time.time()))))
+            with open("/Users/hdy/Desktop/yanzhengma.jpg", 'wb') as f:
+                f.write(r.content)
+            code = input("请输入验证码")
+            # 获取到验证码
+            # code = code.encode('unicode_escape')
+            code = quote(code, safe = string.printable)
+            form_data = {
+                'account': username,
+                'password': encrypt_password,
+                'captcha': code,
+                'keysTime': data_keys_time
+            }
+            r = session.post("https://www.yiban.cn/login/doLoginAjax", data=form_data,allow_redirects=False)
+            login_json = json.loads(r.text)
+            if(login_json['code'] == 200):
+                print("模拟登陆成功")
+                print(login_json)
+                user_website = login_json['data']['url']
+                user_id = user_website[user_website.index("_id")+4:]
+                addFeed()
+                # 循环四次.
+                for i in range(4):
+                    addblog()
+                addYiMiaoMiao()
+            else:
+                print("再次登录失败")
     # print(r.text)
 
 # 密码进行rsa加密
@@ -131,6 +164,7 @@ def addblog():
             # blogid: 12300216 oid: 18884862 mid: 48893712 reply_user_id: 0 reply_comment_id: 0 content: 123123123
             session.post("http://www.yiban.cn/blog/blog/addcomment/", data={
                          "blogid": blog_id, "oid": user_id, "mid": Mount_id, "reply_user_id": "0", "reply_comment_id": "0", "content": randomstr})
+            print("博文发表成功")
         else:
             print("获取请求的博文错误...")
 
@@ -148,6 +182,16 @@ def addYiMiaoMiao():
     r = session.post("http://ymm.yiban.cn/article/index/add", json=data_form)
     print(r.text)
 
+
+# 网站\客户端查看(个人/公共/机构账号)主页
+def addPersonWebsite():
+    # 查看个人
+    session.get("http://www.yiban.cn/user/index/index/user_id/"+user_id)
+    # 查看机构
+    session.get("http://www.yiban.cn/school/index/id/5000090")
+    # 查看公共
+    session.get("http://www.yiban.cn/user/index/index/user_id/15977811")
+    print("网站\客户端查看(个人/公共/机构账号)主页完成")
 
 # try:
 login('13216151732', 'h378759617')
